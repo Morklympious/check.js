5c09e9fdc33da785e6accd7ed576b962c50dbe03
Bradley Stafford <Bradleypstafford@gmail.com>
2016-01-14 19:07:57 -0800

    delete gruntfile, remove dependencies, split code into components to begin work

--- src/components/is-core.js
+++ src/components/is-core.js
@@ -1,61 +1,24 @@
 // is.js Core
 
-var root = this || global;
-var previousIs = root.is;
-
-// define 'is' object and current version
-var is = {};
-is.VERSION = '0.7.6';
-
-// define interfaces
-is.not = {};
-is.all = {};
-is.any = {};
-
-// cache some methods to call later on
-var toString = Object.prototype.toString;
-var arraySlice = Array.prototype.slice;
-var hasOwnProperty = Object.prototype.hasOwnProperty;
-
 // helper function which reverses the sense of predicate result
-function not(func) {
-    return function() {
-        return !func.apply(null, arraySlice.call(arguments));
-    };
+function not(predicate) {
+  return !predicate();
 }
 
-// helper function which call predicate function per parameter and return true if all pass
+// all(isOdd, isEven, isWhatever)
 function all(func) {
-    return function() {
-        var parameters = arraySlice.call(arguments);
-        var length = parameters.length;
-        if(length === 1 && is.array(parameters[0])) {    // support array
-            parameters = parameters[0];
-            length = parameters.length;
-        }
-        for (var i = 0; i < length; i++) {
-            if (!func.call(null, parameters[i])) {
-                return false;
-            }
-        }
-        return true;
-    };
+    var args = Array.prototype.slice.call(arguments);
+
+    return args.every(function(predicate) {
+      return predicate();
+    });
 }
 
 // helper function which call predicate function per parameter and return true if any pass
 function any(func) {
-    return function() {
-        var parameters = arraySlice.call(arguments);
-        var length = parameters.length;
-        if(length === 1 && is.array(parameters[0])) {    // support array
-            parameters = parameters[0];
-            length = parameters.length;
-        }
-        for (var i = 0; i < length; i++) {
-            if (func.call(null, parameters[i])) {
-                return true;
-            }
-        }
-        return false;
-    };
+  var args = Array.prototype.slice.call(arguments);
+
+  return args.some(function(predicate) {
+    return predicate();
+  });
 }
