2cb8ad27ed0e91079d402da95573485e09c85cc7
Bradley Stafford <morklympious@gmail.com>
2016-02-23 16:15:33 -0800

    Add test cases for array

---
+++ test/array.js
@@ -0,0 +1,62 @@
+var expect = require('chai').expect,
+    array  = require('../src/components/array.js');
+
+    describe('Array Checking', function() {
+
+      describe('contains(collection, value)', function() {
+        var contains = array.contains;
+
+        it('should be a function', function(){
+          expect(contains).to.be.a('function');
+        });
+
+        it('should return false on empty array', function(){
+          expect(contains([], 1)).to.be.false;
+        });
+
+        it('should return false on array without matching element', function(){
+          expect(contains([1, 2, 3], 5)).to.be.false;
+        })
+
+        it('should return true on array with matching element', function(){
+          expect(contains([1, 2, 3], 3)).to.be.true;
+        })
+
+      });
+
+
+      describe('empty(collection)', function() {
+        var empty = array.empty;
+
+
+        it('should be a function', function(){
+          expect(empty).to.be.a('function');
+        });
+
+        it('should return true for empty array', function(){
+          expect(empty([])).to.be.true;
+        })
+
+        it('should return false on array with elements', function(){
+          expect(empty([1, 2, 3])).to.be.false;
+        })
+      });
+
+
+      describe('sorted(collection)', function() {
+        var sorted = array.sorted;
+
+        it('should be a function', function(){
+          expect(sorted).to.be.a('function');
+        });
+
+        it('should return true on sorted array', function() {
+          expect(sorted([1, 2, 3])).to.be.true;
+        })
+
+        it('should return false on unsorted array', function() {
+          expect(sorted([1, 3, 2])).to.be.false;
+        })
+      });
+
+    });
